9ddb810f0ccf7eec0c148506ead2b93a538077bb
Zoey Farago <zf.zoodle@gmail.com>
2021-01-14 02:36:43 +0000

    Wordsearch pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+//Pair programmed with Gabe, Idel, and Jacob
+const wordSearch = (letters, word) => {
+  //check for rows
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  // go through 2d array column by column and build a new array
+  let newArray = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    let joinedColumn = [];
+    for (let j = 0; j < letters.length; j++) {
+      joinedColumn.push(letters[j][i]);
     }
-}
+    newArray.push(joinedColumn);
+  }
+ 
+  // newArray after join looks like this:
+  // newArray = ["ASYHWBUOE", "WEFMHFBDZ", "CICJCRTCK"]
+    
+  // check for columns
+  const columnJoin = newArray.map(ls => ls.join(''));
+  for (let l of columnJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
